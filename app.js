961c49aa68d027081b2243a8b1e789104275c4d0
AniketSharma-dev <aniketsharma.xo@gmail.com>
2024-11-10 00:39:31 +0530

    User Register Funtion

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const userRouter = require("./routes/user.routes");
 const app = express();
 
 const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.set("view engine", "ejs");
 
 app.use("/user", userRouter);
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,8 +2,13 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/test", (req, res) => {
-  res.send("User Test Router");
+router.get("/register", (req, res) => {
+  res.render("register");
+});
+
+router.post("/register", (req, res) => {
+  console.log(req.body);
+  res.send("User Registered");
 });
 
 module.exports = router;
